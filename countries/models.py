42249f73691e9398e755c0dc3144615c1c5dc53c
Nyambura🌻 <nyambura.bianca23@gmail.com>
2023-01-10 01:20:38 +0300

    countries app

---
+++ countries/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Country(models.Model):  
+    name = models.CharField(max_length=100)
+    capital = models.CharField(max_length=100)
+    area = models.IntegerField(help_text="Area in square kilometers")
+
+    def __str__(self):
+        return self.name
